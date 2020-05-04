0f958dfa47b01f630c66c3ec8fe679906511a224
Kiichi Tachibana <bodega1123@gmail.com>
2020-05-04 22:17:21 +0900

    Add query to fetch data

--- src/components/Todo/TodoPrivateList.js
+++ src/components/Todo/TodoPrivateList.js
@@ -1,42 +1,45 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, Fragment } from 'react';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
 
-import TodoItem from "./TodoItem";
-import TodoFilters from "./TodoFilters";
+import TodoItem from './TodoItem';
+import TodoFilters from './TodoFilters';
 
-const TodoPrivateList = props => {
+const GET_MY_TODOS = gql`
+  query getMyTodos {
+    todos(
+      where: { is_public: { _eq: false } }
+      order_by: { created_at: desc }
+    ) {
+      id
+      title
+      created_at
+      is_completed
+    }
+  }
+`;
+
+const TodoPrivateList = (props) => {
   const [state, setState] = useState({
-    filter: "all",
+    filter: 'all',
     clearInProgress: false,
-    todos: [
-      {
-        id: "1",
-        title: "This is private todo 1",
-        is_completed: true,
-        is_public: false
-      },
-      {
-        id: "2",
-        title: "This is private todo 2",
-        is_completed: false,
-        is_public: false
-      }
-    ]
   });
 
-  const filterResults = filter => {
+  const filterResults = (filter) => {
     setState({
       ...state,
-      filter: filter
+      filter: filter,
     });
   };
 
   const clearCompleted = () => {};
+  const { todos } = props;
 
-  let filteredTodos = state.todos;
-  if (state.filter === "active") {
-    filteredTodos = state.todos.filter(todo => todo.is_completed !== true);
-  } else if (state.filter === "completed") {
-    filteredTodos = state.todos.filter(todo => todo.is_completed === true);
+  let filteredTodos = todos;
+  if (state.filter === 'active') {
+    filteredTodos = todos.filter((todo) => todo.is_completed !== true);
+  } else if (state.filter === 'completed') {
+    filteredTodos = todos.filter((todo) => todo.is_completed === true);
   }
 
   const todoList = [];
@@ -61,4 +64,20 @@ const TodoPrivateList = props => {
   );
 };
 
-export default TodoPrivateList;
+const TodoPrivateListQuery = () => {
+  const { loading, error, data } = useQuery(GET_MY_TODOS);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    console.error(error);
+    return <div>Error!</div>;
+  }
+
+  return <TodoPrivateList todos={data.todos} />;
+};
+
+export default TodoPrivateListQuery;
+export { GET_MY_TODOS };
