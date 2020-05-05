a41d5ff7814f43e5c8001dd18a1bec0be447b584
Kiichi Tachibana <bodega1123@gmail.com>
2020-05-05 20:33:35 +0900

    Add useMutation to update cache

--- src/components/Todo/TodoInput.js
+++ src/components/Todo/TodoInput.js
@@ -1,14 +1,68 @@
-import React from "react";
+import React, { useState } from 'react';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/react-hooks';
+
+import { GET_MY_TODOS } from './TodoPrivateList';
+
+const ADD_TODO = gql`
+  mutation($todo: String!, $isPublic: Boolean!) {
+    insert_todos(objects: { title: $todo, is_public: $isPublic }) {
+      affected_rows
+      returning {
+        id
+        title
+        created_at
+        is_completed
+      }
+    }
+  }
+`;
 
 const TodoInput = ({ isPublic = false }) => {
+  const updateCache = (cache, { data }) => {
+    if (isPublic) {
+      return null;
+    }
+
+    const existingTodos = cache.readQuery({
+      query: GET_MY_TODOS,
+    });
+
+    const newTodo = data.insert_todos.returning[0];
+    cache.writeQuery({
+      query: GET_MY_TODOS,
+      data: {
+        todos: [newTodo, ...existingTodos.todos],
+      },
+    });
+  };
+
+  let input;
+  const [todoInput, setTodoInput] = useState('');
+
+  const resetInput = () => {
+    setTodoInput('');
+  };
+
+  const [addTodo] = useMutation(ADD_TODO, {
+    update: updateCache,
+    onCompleted: resetInput,
+  });
   return (
     <form
       className="formInput"
-      onSubmit={e => {
+      onSubmit={(e) => {
         e.preventDefault();
+        addTodo({ variables: { todo: todoInput, isPublic } });
       }}
     >
-      <input className="input" placeholder="What needs to be done?" />
+      <input
+        className="input"
+        placeholder="What needs to be done?"
+        value={todoInput}
+        onChange={(e) => setTodoInput(e.target.value)}
+        ref={(n) => (input = n)}
+      />
       <i className="inputMarker fa fa-angle-right" />
     </form>
   );
